e19452b95d1319a402287676b02d1fc5aedbad9c
Nihar Ogale <ogalenihar@gmail.com>
2025-05-11 21:08:15 -0700

    added auth client, navbar and auth provider component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,6 @@
 import '@styles/globals.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 
 export const metadata = {
     title: "Promptopia",
@@ -9,13 +11,15 @@ const RootLayout = ({children}) => {
   return (
     <html lang='en'>
         <body>
+          <Provider>
             <div className='main'>
                 <div className='gradient'/>
             </div>
-
             <main className='app'>
+              <Nav />
               {children}
-            </main>
+          </main>
+          </Provider>
         </body>
     </html>
   )
